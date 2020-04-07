b092059ccdf279b55db4f159cf663a61cbe69234
Anupam95 <anupam.singla21@gmail.com>
2020-04-07 11:58:35 +0530

    steup completed with angular

--- demo/src/main/java/com/mars/demo/repository/BookRepository.java
+++ demo/src/main/java/com/mars/demo/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package com.mars.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.mars.demo.entitiy.Book;
 
+@CrossOrigin("*")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
